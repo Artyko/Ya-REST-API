32fcbd2101d90f61424ce2fe3578d2402c5d3197
Artem Kovyryagin <dastet@rambler.ru>
2019-10-02 21:07:28 +0300

    add citizen model without validate

--- gift_shop/gift_shop/REST_API/models.py
+++ gift_shop/gift_shop/REST_API/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import int_list_validator
+from datetime import datetime
 
-# Create your models here.
+GENDER_CHOISES = ['male', 'female']
+
+
+class Citizens(models.Model):
+    import_id = models.AutoField(primary_key=True)
+    citizen_id = models.PositiveIntegerField()
+    town = models.CharField(max_length=256)
+    street = models.CharField(max_length=256)
+    building = models.CharField(max_length=256)
+    apartment = models.PositiveIntegerField()
+    name = models.CharField(max_length=256)
+    birth_date = models.DateField(input_formats=['%dd/%mm/%YYYY'])  # need date validator
+    gender = models.CharField(choices=GENDER_CHOISES)
+    relatives = models.CharField(validators=int_list_validator)
+
+    class Meta:
+        ordering = ['created']
+
